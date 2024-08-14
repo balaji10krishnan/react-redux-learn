ce88c38e88c23be1c96046a99288d9ef1ebb9744
balajikmacapp <balajik@macappstudio.com>
2024-08-14 21:49:25 +0530

    basic routing set

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,30 @@
 import { useState } from "react";
-
+import { BrowserRouter, NavLink, Route, Routes } from "react-router-dom";
+import Home from "./component/Home";
+import Course from "./component/Course";
+import Playlists from "./component/Playlists";
+import PlaylistDetail from "./component/PlaylistDetail";
 import "./App.css";
-
 function App() {
-  return <div></div>;
+  return (
+    <>
+      <BrowserRouter>
+        <header>
+          <h2>Redux Learning</h2>
+          <nav>
+            <NavLink to={"/"}>Home</NavLink>
+            <NavLink to={"/playlist"}>Playlist</NavLink>
+          </nav>
+        </header>
+        <Routes>
+          <Route index element={<Home />} />
+          <Route path="/course" element={<Course />} />
+          <Route path="/playlists" element={<Playlists />} />
+          <Route path="/playlist" element={<PlaylistDetail />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
---
+++ src/component/PlaylistDetail.jsx
@@ -0,0 +1,5 @@
+const PlaylistDetail = () => {
+  return <h1>PlaylistDetail</h1>;
+};
+
+export default PlaylistDetail;
